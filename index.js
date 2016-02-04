f36043dcd59b28b7fce6d049f6642ff7ec054b6f
shoom <shoom3301@gmail.com>
2016-02-04 13:58:46 +0600

    deploy

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by shoom on 04.02.16.
+ */
+
+var http = require('http');
+
+var server = http.createServer(function(request, response){
+    response.end('CATCH N RUN');
+});
+
+server.listen(80, function(){
+    console.log("Server started");
+});
